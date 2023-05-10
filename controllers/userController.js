4c57b3d0394fee0e066d88ce2d77d7fa8e93cb02
Wiktor Tarnowski <MrBlack1223@wp.pl>
2023-05-10 03:43:52 +0200

    Fixing bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,7 +44,7 @@ export const updateUser = async(req,res) =>{
         try{
             const user = await User.findById(req.params.userID)
             await user.updateOne(req.body)
-            res.status(200).jsno(user)
+            res.status(200).json(user)
         }catch(error){
             res.send(error)
         }
